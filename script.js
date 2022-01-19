ebab1bbc1edfbe03a244e00d8f230b83c477492f
Yur1-Gomes <yuri2912@hotmail.com>
2022-01-18 21:21:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+for( let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+let i = 1;
+while(i <=10){
+    console.log(i);
+    i++;
+}
+
+for(let i = 1; i <= 100; i += 2){
+    console.log(i);
+}
+
+let conta = 1;
+while(conta <= 100){
+    console.log(conta);
+    conta += 2;
+}
+
+for(let conta = 0; conta <= 100; conta += 2){
+    console.log(conta);
+}
+
+let $i = 0;
+while($i <= 100){
+    console.log($i);
+    $i += 2;
+}
